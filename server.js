2835069fa5397623a043c2cb635cf4922ba43ed5
Joshua <legioncommander555@gmail.com>
2021-02-05 23:06:06 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+    () => console.log("Connected to DB")
+)
+
+//import route
+const usersRoute = require('./routes/users');
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//Route middleware
+app.use('/api/users', usersRoute);
+
+if(process.env.NODE_ENV === 'production') {
+    app.get('/', function (req, res) {
+        res.sendFile('/index.html', { root: __dirname });
+    });
+}
+
+app.listen(port, () => console.log("Server is running"));
